c9b763dbc7f120d531fba4e51f6d13fe85d4de23
zzaengee <163216738+zzaengee@users.noreply.github.com>
2024-03-27 14:25:01 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+// 맨 위로 이동 버튼
+let moveToTop = function () {
+    document.body.scrollIntoView({ behavior: "smooth" });
+  };
+  
+  // 타이핑 효과
+  const $txt = document.querySelector(".txt-title");
+  const content = "0328\nHAPPY BIRTHDAY \n CHANWOO \n🎉🎉🎉🎉🎉🎉🎉";
+  let contentIndex = 0;
+  
+  let typing = function () {
+    $txt.innerHTML += content[contentIndex];
+    contentIndex++;
+    if (content[contentIndex] === "\n") {
+      $txt.innerHTML += "<br />";
+      contentIndex++;
+    }
+    if (contentIndex > content.length) {
+      $txt.textContent = "";
+      contentIndex = 0;
+    }
+  };
+  
+  setInterval(typing, 200);
+  
+  // 이미지 슬라이드
+  let imgIndex = 0;
+  let position = 0;
+  const IMG_WIDTH = 400;
+  const $btnPrev = document.querySelector(".btn-prev");
+  const $btnNext = document.querySelector(".btn-next");
+  const $slideImgs = document.querySelector(".slide-images");
+  
+  let prev = function () {
+    if (imgIndex > 0) {
+      $btnNext.removeAttribute("disabled");
+      position += IMG_WIDTH;
+      $slideImgs.style.transform = `translateX(${position}px)`;
+      imgIndex = imgIndex - 1;
+    }
+    if (imgIndex == 0) {
+      $btnPrev.setAttribute("disabled", "true");
+    }
+  };
+  
+  let next = function () {
+    if (imgIndex < 5) {
+      $btnPrev.removeAttribute("disabled");
+      position -= IMG_WIDTH;
+      $slideImgs.style.transform = `translateX(${position}px)`;
+      $slideImgs.style.transition = "transform .5s ease-out";
+      imgIndex = imgIndex + 1;
+    }
+    if (imgIndex == 4) {
+      $btnNext.setAttribute("disabled", "true");
+    }
+  };
+  
+  let init = function () {
+    $btnPrev.setAttribute("disabled", "true");
+    $btnPrev.addEventListener("click", prev);
+    $btnNext.addEventListener("click", next);
+  };
+  
+  init();
+  
+  // 모달
+  const $modalBg = document.getElementsByClassName("modal-background");
+  const $btnOpen = document.getElementsByClassName("btn-open");
+  const $btnClose = document.getElementsByClassName("btn-close");
+  
+  function modal(num) {
+    $btnOpen[num].addEventListener("click", () => {
+      $modalBg[num].style.display = "flex";
+      document.body.style.overflow = "hidden";
+    });
+    $btnClose[num].addEventListener("click", () => {
+      $modalBg[num].style.display = "none";
+      document.body.style.overflow = "unset";
+    });
+  }
+  
+  for (let i = 0; i < $btnOpen.length; i++) {
+    modal(i);
+  }
+  
+  // 스크롤바
+  let scrollTop = 0;
+  let bar = document.getElementsByClassName("bar-ing")[0];
+  
+  window.addEventListener(
+    "scroll",
+    () => {
+      scrollTop = document.documentElement.scrollTop;
+      let per = Math.ceil(
+        (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+      );
+      bar.style.width = per + "%";
+    },
+    false
+  );
